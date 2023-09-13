d9d8d6ae930a389e68c64c1eb2f207d7cb63b07a
caian maestrelli <djcaian@gmail.com>
2023-09-12 22:29:30 -0300

    async-await-cep-app-testes-assincronos

--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
-const btnSearch = document.querySelector('button');
-const inputEl = document.querySelector('input');
+import getAddressFromCep from './getAddressFromCep';
+document.querySelector('button').addEventListener('click', handleClick);
 
-const endereco = document.querySelector('pre');
+export async function handleClick() {
+	const cep = document.querySelector('input').value;
 
-btnSearch.addEventListener('click', handleClick);
-
-async function handleClick() {
-	const cep = inputEl.value;
-	try{
-		const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-		const data = await response.json();
-
-		endereco.innerHTML = JSON.stringify(data);
-		return data;
-	}catch(error) {
-		return error.mesaage;
+	try {
+		const addressData = await getAddressFromCep(cep);
+		document.querySelector('pre').innerHTML = JSON.stringify(addressData);
+	} catch (error) {
+		alert(error.message);
 	}
 }
+// async function handleClick() {
+// 	const cep = inputEl.value;
+// 	try{
+// 		const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+// 		const data = await response.json();
+
+// 		endereco.innerHTML = JSON.stringify(data);
+// 		return data;
+// 	}catch(error) {
+// 		return (
+// 			Swal.fire({
+// 				alert: 'error',
+// 				icon: 'error',
+// 				text: 'Digite um CEP válido',
+// 			})
+// 		);
+// 	}
+// }
